0dff126f5ab659d694a133751ec3f2c9dca261d4
louazz <louaizaiter@gmail.com>
2023-05-17 12:52:13 +0100

    change

--- src/controllers/compile.ts
+++ src/controllers/compile.ts
@@ -23,25 +23,20 @@ export const compileFile= async (
   },
 ) => {
   const id = params.docId;
-
-  let src = "./src/uploads/" + id + "/latex.tex";
-
-  // Arguments can be either a single String or in an Array
-  let args = "-f latex -t pdf -o ./src/uploads/" + id + "/latex.pdf";
-
-  // Set your callback function
-  const callback = (err: any, result: any) => {
-    if (err) console.error("Oh Nos: ", err);
-    return console.log(result), result;
-  };
-
-  // Call pandoc
-  nodePandoc(src, args, callback);
+  
+  let cmd = new Deno.Command("pdflatex", { args: ['-output-directory=./src/uploads/'+id,'-jobname=latex', './src/uploads/'+id+'/latex.tex'] });
+   let { stdout, stderr } = await cmd.output();
+// stdout & stderr are a Uint8Array
+console.log(new TextDecoder().decode(stdout));
 let file=null;
   try{
    file = await Deno.readFile("./src/uploads/" + id + "/latex.pdf");}catch(error){
     
-    nodePandoc(src, args, callback);
+    let { stdout, stderr } = await cmd.output();
+    // stdout & stderr are a Uint8Array
+    console.log(new TextDecoder().decode(stdout));
+    file = await Deno.readFile("./src/uploads/" + id + "/latex.pdf");
+
   }
   const head = new Headers();
   head.set("content-type", "application/pdf");
